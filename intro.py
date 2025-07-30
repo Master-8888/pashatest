4809913e7221df20b163d4019e27d811a6ec85ba
Павел <your_email@example.com>
2025-07-30 18:38:31 +0300

    new

---
+++ intro.py
@@ -0,0 +1,16 @@
+print('test text')
+print("test text      '      ")
+print("""test text      ' " '     """)
+
+print(chr(68))  # узнат ьпо номерку какй это симвал
+print(ord('D'))
+
+print('🥁')
+print(ord('🥁'))  # функция по которой можно узнать номер символа
+
+print('\U00002602')
+print('\u0420')
+
+firstname = 'Pasha'
+print(firstname)
+print(id(firstname))
