c6319f96812f8ba350d912d26b0ef72135a97154
Павел <your_email@example.com>
2025-07-30 22:31:45 +0300

    dz4

---
+++ main.py
@@ -0,0 +1,18 @@
+import poems
+import sign
+
+avtor = sign.AVTOR.format(name='Pavel')
+print(avtor)
+
+wishes = "Більше практики!\nЦікаві проекти!"
+print(wishes)
+
+stars = "**************************************************************************"
+print(stars)
+
+print(poems.SHEVCHENKO146)
+
+print(stars)
+
+print(poems.SHEVCHENKO246)
+print(stars)
