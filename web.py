908933a35971a32ff5754041f317c5bb191dead3
Павел <your_email@example.com>
2025-07-31 16:04:04 +0300

    dz5

---
+++ web.py
@@ -0,0 +1,12 @@
+# очікуваний результат у вигляді: My name is David, I am 14 years old👣
+
+# example
+# f = '\N{Footprints}'  # not informative naming, the correct code below
+smile_footprint = '\U0001F463'
+
+user_name = 'Max'
+user_age ='14'
+result = f'My name is {user_name}, I am {user_age} years old {smile_footprint}'
+
+print (result)
+
